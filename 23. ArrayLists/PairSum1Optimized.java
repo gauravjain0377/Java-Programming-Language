7987d43bd169ae3e4d5ba29a4e59d8caf7d94661
Gaurav Jain <jaingaurav906@gmail.com>
2025-12-31 00:41:40 +0530

    Add ArrayList algorithm examples and reorganize files
    Added new Java files demonstrating ArrayList algorithms: container with most water (brute force and optimized), finding maximum, multi-dimensional ArrayLists, pair sum (brute force, optimized, and for rotated arrays), sorting, and swapping elements. Also reorganized basic ArrayList operation files into a dedicated 'Operations of ArrayList' directory for better structure.

---
+++ 23. ArrayLists/PairSum1Optimized.java
@@ -0,0 +1,44 @@
+// Pair Sum - 1
+// Find if any pair in a Sorted ArrayList has a target sum.
+// 2 Pointer Approach
+
+import java.util.*;
+
+public class PairSum1Optimized {
+
+    // 2 Pointer Approach
+    public static boolean pairSum1(ArrayList<Integer> list, int target) {
+        int lp = 0;                // left pointer
+        int rp = list.size()-1;    // right pointer
+
+        while(lp != rp) {
+            // case 1
+            if(list.get(rp) + list.get(rp) == target) {
+                return true;
+            } 
+
+            // case 2
+            if(list.get(rp) + list.get(rp) < target) {
+                lp++;
+            } else {
+                // case 3
+                rp--;
+            }
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        int target = 6;
+
+        System.out.println(pairSum1(list, target));
+    }
+}
