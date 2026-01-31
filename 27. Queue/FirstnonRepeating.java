dc93cb44a82c422d7c1e696d03c1b1c1e8eaf3c8
Gaurav Jain <jaingaurav906@gmail.com>
2026-01-31 17:13:35 +0530

    Implement stack and queue using auxiliary data structures
    Added Stackusing2Queues and FirstnonRepeating classes. Implemented queue using two stacks and linked list, and stack using two queues. Removed placeholder Queueusing2Queues. Enhanced QueueusingLL and Queueusing2Stacks with full implementations and test code.

---
+++ 27. Queue/FirstnonRepeating.java
@@ -0,0 +1,16 @@
+/* First non-repeating letter in a stream of characters. (lowercase characters)
+     aabccxb -> a -1 b b b b x
+  a - a
+  aa - -1
+  aab - b
+  aabc - b 
+  aabcc - b 
+  aabccx - b 
+  aabcxb - x
+  */
+
+public class FirstnonRepeating {
+    public static void main(String[] args) {
+        
+    }
+}
