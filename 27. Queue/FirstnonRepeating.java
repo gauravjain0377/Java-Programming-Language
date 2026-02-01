a56d0dc3e6de57851985ed791aa8d8e39cc10777
Gaurav Jain <jaingaurav906@gmail.com>
2026-02-02 00:31:38 +0530

    Implement first non-repeating character in stream
    Added logic to FirstnonRepeating.java to print the first non-repeating character at each step in a character stream using a queue and frequency array. Also added a new Interleave.java file with a class stub and problem description for interleaving two halves of a queue.

--- 27. Queue/FirstnonRepeating.java
+++ 27. Queue/FirstnonRepeating.java
@@ -7,10 +7,45 @@
   aabcc - b 
   aabccx - b 
   aabcxb - x
+
+
+  1. frequency[] = [26] 'a'-'z'
+  2. Queue <characters>
+  for(int i=0; str.length()) {
+  ch = str.charAt(i)
+  q.add(ch)
+  freq[ch-'a']++
+  
+  q.remove() = till i find 1st non-repeating letter
+  if(q.empty()) -> -1}
   */
+  
+import java.util.*;
 
 public class FirstnonRepeating {
+    public static void printNonRepeating(String str) {
+        int freq[] = new int[26];
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+
+            while(!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+                q.remove();
+            }
+
+            if(q.isEmpty()) {
+                System.out.println(-1 + " ");
+            } else {
+                System.out.println(q.peek() + " ");
+            }
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        String str = "aabccxb";
+        printNonRepeating(str);
     }
 }
---
+++ 27. Queue/Interleave.java
@@ -0,0 +1,10 @@
+/* Interleave Means ek dusre ke andar jodhna
+Interleave 2 Halves of a Queue(even length)
+1 2 3 4 5 6 7 8 9 10
+1 6 2 7 3 8 4 9 5 10 
+*/ 
+
+
+public class Interleave {
+    
+}
