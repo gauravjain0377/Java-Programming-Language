d9c25c8d55ab8d2a7b44cd8007a229896c79126f
Gaurav Jain <jaingaurav906@gmail.com>
2026-02-11 00:26:57 +0530

    BST

---
+++ 32. Binary Search Trees (Part 1)/Search.java
@@ -0,0 +1,14 @@
+public class Search {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
