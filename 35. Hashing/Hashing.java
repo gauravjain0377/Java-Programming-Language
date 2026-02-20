c21a4d267f6e19ad402ad235e7c494f4acc11eab
Gaurav Jain <jaingaurav906@gmail.com>
2026-02-20 16:56:18 +0530

    Update Hashing.java

--- 35. Hashing/Hashing.java
+++ 35. Hashing/Hashing.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+
 public class Hashing {
     public static void main(String[] args) {
-        
+        // Create 
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        // Insert - O(1)
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("US", 50);  
+
+        System.out.println(hm);
+
+        // Get - O(1)
+        int population = hm.get("India");
+        System.out.println(population);
     }
 }
